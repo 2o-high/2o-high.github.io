2f43a9d702fe3e13b97aa7f9b7038defe57dcafe
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2020-03-23 16:19:31 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
-let imgLink = 'https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg';
-let contents = JSON.parse(`{"items": [["Άρθρο 1", "Lorem ipsum", "1.html", "${imgLink}"], ["Άρθρο 2", "Dolor sit amet", "2.html", "${imgLink}"], ["Άρθρο 3", "Consectetur adipisicing elit", "3.html", "${imgLink}"], ["Άρθρο 4", "Consectetur adipisicing elit", "4.html", "${imgLink}"]]}`);
+let contents = JSON.parse(`{"items": [["Άρθρο 1", "Lorem ipsum", "1.html", "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fgetwallpapers.com%2Fwallpaper%2Ffull%2F5%2Fa%2Fe%2F74906.jpg&f=1&nofb=1"],
+                                      ["Άρθρο 2", "Dolor sit amet", "2.html", "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fgetwallpapers.com%2Fwallpaper%2Ffull%2F7%2F7%2F0%2F74728.jpg&f=1&nofb=1"],
+                                      ["Άρθρο 3", "Consectetur adipisicing elit", "3.html", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FJPJjs2apRGA%2Fmaxresdefault.jpg&f=1&nofb=1"],
+                                      ["Άρθρο 4", "Consectetur adipisicing elit", "4.html", "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.wallpapersafari.com%2F4%2F64%2FXAc8zt.jpg&f=1&nofb=1"]]}`);
 let hash = window.location.hash.substring(1);
 let currentTab = '#main';
 
@@ -9,7 +11,7 @@ $(document).ready(() => {
 	
 	let index = 0;
 	for (let item in contents.items) {
-		let code = `<div class="slideanim card" onclick="window.location = '${entries[item][2]}'">
+		let postCode = `<div class="slideanim card" onclick="window.location = '${entries[item][2]}'">
 					<div class="row">
 						<div class="col-sm-6">
 							<img src="${entries[item][3]}" class="entryImg"></img>
@@ -21,13 +23,26 @@ $(document).ready(() => {
 						</div>
 					</div>
 				</div>
-				<br>`;
+                <br>`;
+                
+        let carouselCode = `<div class="carousel-item">
+                                <img src="${entries[item][3]}" width="1100" height="500">
+                                <div class="carousel-caption">
+                                    <h3>${entries[item][0]}</h3>
+                                    <p>${entries[item][1]}</p>
+                                </div>   
+                            </div>`;
 
-		if (index <= 2) $('#main').append(code);
-		$('#posts').append(code);
+		if (index <= 2) {
+            $('.carousel-inner').append(carouselCode);
+            $('#main').append(postCode);
+        }
+
+		$('#posts').append(postCode);
 		index++;
 	}
-	
+    
+    $('.carousel-item:first').addClass('active');
 	$('#main').append(`<br><center><button type="button" class="btn btn-primary" onclick="showTab('posts')">Δείτε περισσότερα</button></center><br>`);
 	
 	$('footer a').on('click', function(event) {
