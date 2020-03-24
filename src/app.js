fab4abfe213f05a20050b8d64b9d1fd69b398dcd
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2020-03-24 09:43:26 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,23 +6,23 @@ let hash = window.location.hash.substring(1);
 let currentTab = '#main';
 
 $(document).ready(() => {
-	if (hash !== "") showTab(hash);
-	let entries = contents.items;
-	
-	let index = 0;
-	for (let item in contents.items) {
-		let postCode = `<div class="slideanim card" onclick="window.location = '${entries[item][2]}'">
-					<div class="row">
-						<div class="col-sm-6">
-							<img src="${entries[item][3]}" class="entryImg"></img>
-						</div>
-						<div class="col-sm-6">
-							<h1 class="display-4">${entries[item][0]}</h1>
-							<hr>
-							<p>${entries[item][1]}</p>
-						</div>
-					</div>
-				</div>
+    if (hash !== "") showTab(hash);
+    let entries = contents.items;
+    
+    let index = 0;
+    for (let item in contents.items) {
+        let postCode = `<div class="slideanim card" onclick="window.location = '${entries[item][2]}'">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <img src="${entries[item][3]}" class="entryImg"></img>
+                        </div>
+                        <div class="col-sm-6">
+                            <h1 class="display-4">${entries[item][0]}</h1>
+                            <hr>
+                            <p>${entries[item][1]}</p>
+                        </div>
+                    </div>
+                </div>
                 <br>`;
                 
         let carouselCode = `<div class="carousel-item">
@@ -33,63 +33,63 @@ $(document).ready(() => {
                                 </div>   
                             </div>`;
 
-		if (index <= 2) {
+        if (index <= 2) {
             $('.carousel-inner').append(carouselCode);
             $('#main').append(postCode);
         }
 
-		$('#posts').append(postCode);
-		index++;
-	}
+        $('#posts').append(postCode);
+        index++;
+    }
     
     $('.carousel-item:first').addClass('active');
-	$('#main').append(`<br><center><button type="button" class="btn btn-primary" onclick="showTab('posts')">Δείτε περισσότερα</button></center><br>`);
-	
-	$('footer a').on('click', function(event) {
-		if (this.hash !== "") {
-			event.preventDefault();
-			let hash = this.hash;
-			
-			$('html, body').animate({
-				scrollTop: $(hash).offset().top
-			}, 900, () => {
-				window.location.hash = hash;
-			});
-		}
-	});
-	
-	$('.card').hover(function() {
-		$(this).addClass('shadow p-4 mb-4').css('cursor', 'pointer'); 
-	}, function() {
-		$(this).removeClass('shadow p-4 mb-4'); 
-	});
-	
-	slideAnim();
+    $('#main').append(`<br><center><button class="learn-more" onclick="showTab('posts');"><span class="circle" aria-hidden="true"><span class="icon arrow"></span></span><span class="button-text">Περισσότερα</span></button></center><br>`);
+    
+    $('footer a').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            let hash = this.hash;
+            
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, () => {
+                window.location.hash = hash;
+            });
+        }
+    });
+    
+    $('.card').hover(function() {
+        $(this).addClass('shadow p-4 mb-4').css('cursor', 'pointer'); 
+    }, function() {
+        $(this).removeClass('shadow p-4 mb-4'); 
+    });
+    
+    slideAnim();
 });
 
 $(window).scroll(() => {
-	slideAnim();
+    slideAnim();
 });
 
 slideAnim = () => {
-	$(".slideanim").each(function(){
-		let pos = $(this).offset().top;
-		let winTop = $(window).scrollTop();
-		
-		if (pos < winTop + 600) {
-			$(this).addClass("slide");
-		}
-	});
+    $(".slideanim").each(function(){
+        let pos = $(this).offset().top;
+        let winTop = $(window).scrollTop();
+        
+        if (pos < winTop + 600) {
+            $(this).addClass("slide");
+        }
+    });
 }
 
 showTab = tab => {
-	if (currentTab == tab) return;
-	if ($('tab')[0] !== undefined) return;
+    if (currentTab == tab) return;
+    if ($('tab')[0] !== undefined) return;
 
-	$(currentTab).css('display', 'none');
-	currentTab = `#${tab}`;
-	$(currentTab).css('display', 'block');
-	let pos = $(window).scrollTop();
-	window.location.hash = currentTab;
-	$(window).scrollTop(pos);
+    $(currentTab).css('display', 'none');
+    currentTab = `#${tab}`;
+    $(currentTab).css('display', 'block');
+    let pos = $(window).scrollTop();
+    window.location.hash = currentTab;
+    $(window).scrollTop(pos);
 }
